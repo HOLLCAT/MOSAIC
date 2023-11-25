04cc9fe176d07a9ed14742a40f9de98a39de1ac9
Yiqin Ning <2741787n@student.gla.ac.uk>
2023-11-25 22:51:08 +0000

    Regarding modifications to article.py

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,9 @@
-from uvicorn import run
-
 from fastapi import FastAPI
 from article import article
-if __name__ == '__main__':
-    run('main:app', reload=True)
 
 app = FastAPI()
 app.include_router(article)
+
+if __name__ == '__main__':
+    from uvicorn import run
+    run('main:app', reload=True)
