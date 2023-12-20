37c6998d5d8c7ceaf8a6e61db837681aa1659b4d
Sam Bradley <2729175b@student.gla.ac.uk>
2023-12-20 18:22:06 +0000

    Home, about and help pages made more responsive #43

--- frontend/src/features/help/index.vue
+++ frontend/src/features/help/index.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="justify-center items-center w-full mt-[-60px] p-12.5 flex flex-col min-h-screen">
-        <div class="flex flex-col items-center w-4/5 h-auto rounded-3xl bg-purple justify-center">
-            <h1 class="text-4xl text-white mt-10 font-semibold pt-10">Help</h1>
-            <div class="text-2xl text-white mt-10 text-center px-40 py-20 text-justify">
-                <h2 class="font-semibold text-center">Need any help?</h2><br/>
-                <p>Feel free to contact us</p>
+    <div class="items-center w-full p-4 sm:p-12.5 flex flex-col flex-shrink h-auto min-h-screen justify-center items-center">
+        <div class="flex flex-col items-center rounded-3xl bg-purple w-2/5">
+            <h1 class="text-2xl text-white font-semibold pt-10 xs:text-2xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-4xl">Help</h1>
+            <div class="text-white mt-10 text-center px-40 py-20 text-justify">
+                <h2 class="text-sm font-semibold text-center xs:text-md sm:text-lg md:text-1xl lg:text-2xl xl:text-2xl">Need any help?</h2><br/>
+                <p class="text-sm text-center xs:text-md sm:text-lg md:text-1xl lg:text-2xl xl:text-2xl">Feel free to contact us</p>
             </div>
         </div>
     </div>
