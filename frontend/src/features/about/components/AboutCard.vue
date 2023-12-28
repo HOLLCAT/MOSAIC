3d27fbe1563e3049d0aedcadb802f2091bb6ceed
Sam Bradley <2729175b@student.gla.ac.uk>
2023-12-28 17:46:48 +0000

    Frontend standardisation. Closes #47

---
+++ frontend/src/features/about/components/AboutCard.vue
@@ -0,0 +1,17 @@
+
+<template>
+    <div class="items-center w-full p-4 sm:p-12.5 flex flex-col flex-shrink h-auto min-h-screen justify-center items-center">
+        <div class="flex flex-col items-center rounded-3xl bg-purple w-2/3">
+            <h1 class="text-2xl text-white font-semibold pt-10 xs:text-2xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-4xl">About</h1>
+            <p class="text-xs text-white mt-10 text-center w-4/5 h-2/5 mb-20 text-justify xs:text-base sm:text-md md:text-1xl lg:2xl xl:text-2xl">
+                MOSAIC (Mouse Model Open-Source Information and Archive Center) 
+                is a repository designed to streamline data sharing and integration within the National Mouse Genetics Network. 
+                This network collaborates across various labs and research institutes, employing mouse models to study diseases like cancer and diabetes.
+                MOSAIC addresses the challenge of diverse data formats and distinct lab procedures by offering a unified data portal. 
+                This platform enables scientists to submit their research data, which undergoes a quality control process before being shared. 
+                The portal caters to a variety different types of data, including sequence, image, and behavioural data. 
+            </p>
+        </div>
+    </div>
+</template>
+<script setup lang="ts"></script>
--- frontend/src/features/home/components/DatabankCard.vue
+++ frontend/src/features/home/components/DatabankCard.vue
@@ -1,6 +1,5 @@
 
 <template>
-    <div class="items-center w-full p-4 sm:p-12.5 flex flex-col flex-shrink h-auto min-h-screen justify-center items-center">
         <div class="flex flex-col items-center rounded-3xl bg-purple mb-5 mt-10 justify-between">
             <h1 class="text-base text-white mt-6 font-semibold xs:text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-5xl">Databank</h1>
             <div class="text-xsm flex justify-between text-white text-1xl mt-6 sm:mt-10 px-4 sm:px-10 w-full mb-6 font-medium xs:text-xs sm:text-base md:text-lg lg:text-1xl xl:text-2xl">
@@ -39,42 +38,5 @@
                 </div>
             </div>
         </div>
-        <div class="flex flex-col items-center rounded-3xl bg-purple mb-10 mt-5 justify-between">
-            <h1 class="text-base text-white mt-6 font-semibold xs:text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-5xl">Most Viewed Studies</h1>
-            <div class="w-full p-4">
-                <div class="bg-white rounded-lg p-4 mb-3" v-for="(study, index) in studies" :key="index">
-                    <RouterLink :to="`/study/${study.accession}`" class="text-xs text-gray-700 xs:text-xs sm:text-sm md:text-sm lg:text-base xl:text-ms text-black">
-                        {{ study.title }}
-                    </RouterLink>
-                    <p class="text-xs text-gray-700 xs:text-xs sm:text-sm md:text-sm lg:text-base xl:text-ms">Views: {{ study.releaseViews }}</p>
-                </div>
-            </div>
-        </div>
-    </div>
 </template>
-<script lang="ts">
-import { defineComponent, ref, onMounted } from 'vue';
-import { data } from '@/utils/dummyDataNew';
-
-export default defineComponent({
-    name: 'DataBankCard',
-    setup() {
-        const studies = ref(data);
-
-        const getMostViewedStudies = () => {
-            const sortedData = [...data].sort((a, b) => 
-                (b.releaseViews ? parseInt(b.releaseViews) : 0) - (a.releaseViews ? parseInt(a.releaseViews) : 0)
-            );
-            studies.value = sortedData.slice(0, 3);
-        }
-
-        onMounted(() => {
-            getMostViewedStudies();
-        });
-
-        return {
-            studies
-        };
-    }
-});
-</script>
+<script setup lang="ts"></script>
