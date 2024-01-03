3fd07d317c601f8d188244900c561d36156407b3
Xiaozou Zhu <2721917z@student.gla.ac.uk>
2024-01-03 15:46:14 +0000

    Update database.py

--- backend/database/database.py
+++ backend/database/database.py
@@ -65,6 +65,7 @@ async def update_study_by_id(accession_id: str, study_update: StudyUpdate) -> di
         raise HTTPException(status_code=404, detail="Study not found")
 
     # Only include fields in the update that were explicitly set
+    update_data = study_update.dict(exclude_unset=True)
     for field, value in update_data.items():
         if field=="extra_fields":
             t = value
