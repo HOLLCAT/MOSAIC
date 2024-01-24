e5c9ad7a15c13401c65b5224f9063eaeb3cf7626
Sam Bradley <2729175b@student.gla.ac.uk>
2024-01-24 20:09:11 +0000

    Dark Mode Removed From Website

--- frontend/src/components/SiteFooter.vue
+++ frontend/src/components/SiteFooter.vue
@@ -1,11 +1,11 @@
 <template>
-    <footer class="bg-white shadow dark:bg-purple mt-auto p-4">
+    <footer class="shadow bg-purple mt-auto p-4">
         <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
             <div class="sm:flex sm:items-center sm:justify-between">
                 <a href="" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                    <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">MOSAIC</span>
+                    <span class="self-center text-2xl font-semibold whitespace-nowrap text-white">MOSAIC</span>
                 </a>
-                <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+                <ul class="flex flex-wrap items-center mb-6 text-sm font-medium sm:mb-0 text-gray-400">
                     <li>
                         <router-link to="/about">
                             <span class="hover:underline me-4 md:me-6">About</span>
@@ -21,8 +21,8 @@
                     </li>
                 </ul>
             </div>
-            <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-            <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 MOSAIC. All Rights Reserved.</span>
+            <hr class="my-6 sm:mx-auto border-gray-700 lg:my-8" />
+            <span class="block text-sm  sm:text-center text-gray-400">© 2023 MOSAIC. All Rights Reserved.</span>
         </div>
     </footer>
 </template>
